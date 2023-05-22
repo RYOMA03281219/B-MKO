4b08179e587cf60a303ef4b11d346ce7f840f44f
EC2 Default User <ec2-user@ip-172-31-26-57.ap-northeast-1.compute.internal>
2023-05-22 22:42:10 +0000

    [Add]サインイン,サインアウト後の遷移先の記述

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,10 +1,10 @@
 class Admin::SessionsController < ApplicationController
-  def new
+
+  def after_sign_in_path_for(resource)
+    admin_path
   end
 
-  def create
-  end
-
-  def destroy
+  def after_sign_out_path_for(resource)
+    new_admin_session_path
   end
 end
