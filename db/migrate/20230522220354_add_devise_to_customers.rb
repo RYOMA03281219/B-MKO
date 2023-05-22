d7e50922e2866fb4eeee4944b0b991e463d6ed94
EC2 Default User <ec2-user@ip-172-31-26-57.ap-northeast-1.compute.internal>
2023-05-22 22:43:16 +0000

    [Add]

--- db/migrate/20230522220354_add_devise_to_customers.rb
+++ db/migrate/20230522220354_add_devise_to_customers.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-class AddDeviseToAdmins < ActiveRecord::Migration[6.1]
-  def self.up
-    change_table :admins do |t|
+class AddDeviseToCustomers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :customers do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
@@ -37,10 +37,10 @@ class AddDeviseToAdmins < ActiveRecord::Migration[6.1]
       # t.timestamps null: false
     end
 
-    add_index :admins, :email,                unique: true
-    add_index :admins, :reset_password_token, unique: true
-    # add_index :admins, :confirmation_token,   unique: true
-    # add_index :admins, :unlock_token,         unique: true
+    add_index :customers, :email,                unique: true
+    add_index :customers, :reset_password_token, unique: true
+    # add_index :customers, :confirmation_token,   unique: true
+    # add_index :customers, :unlock_token,         unique: true
   end
 
   def self.down
