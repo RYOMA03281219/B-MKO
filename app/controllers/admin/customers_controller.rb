2e6b1fac15bdb4698c24b3ce2f338a1fe3c2d3f0
EC2 Default User <ec2-user@ip-172-31-26-57.ap-northeast-1.compute.internal>
2023-05-25 17:41:19 +0000

    [Add]indexアクションの追加

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,5 +1,8 @@
 class Admin::CustomersController < ApplicationController
+  before_action :authenticate_admin!
+
   def index
+    @customers = Customer.all.page(params[:page]).per(5)
   end
 
   def show
