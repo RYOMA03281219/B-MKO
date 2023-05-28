ae859afc5f1d36265fbb4c27a6e381be377ce470
EC2 Default User <ec2-user@ip-172-31-26-57.ap-northeast-1.compute.internal>
2023-05-28 12:15:23 +0000

    [Add]サインインとサインアウト機能を追加

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,10 +1,44 @@
-class Public::SessionsController < ApplicationController
-  def new
+class Public::SessionsController < Devise::SessionsController
+  def after_sign_in_path_for(resource)
+    flash[:welcome] = "ようこそ" + current_customer.last_name + "さん！"
+    customers_my_page_path
   end
 
-  def create
+  def after_sign_out_path_for(resource)
+    root_path
   end
 
-  def destroy
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
+
+  # POST /resource/sign_in
+  # def create
+  #   # @customer = Customer.new
+  #   super
+  # end
+
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
+  # 退会しているかを判断するメソッド
+  def customer_state
+  ## 【処理内容1】 入力されたemailからアカウントを1件取得
+    @customer = Customer.find_by(email: params[:customer][:email])
+  ## アカウントを取得できなかった場合、このメソッドを終了する
+    return if !@customer
+  ## 【処理内容2】 取得したアカウントのパスワードと入力されたパスワードが一致してるかを判別
+    if @customer.valid_password?(params[:customer][:password])
+    ## 【処理内容3】
+    end
   end
 end
