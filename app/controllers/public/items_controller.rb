2cb2a80fd628bc843442e42dc14753cd680b6bef
EC2 Default User <ec2-user@ip-172-31-26-57.ap-northeast-1.compute.internal>
2023-05-29 23:54:09 +0000

    [Add]index,new,create,show,edit,updateアクションの追加

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,42 @@
 class Public::ItemsController < ApplicationController
   def index
+    @items = Item.all.page(params[:page]).per(10)
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save!
+      redirect_to item_path(@item_id)
+    else
+      render "new"
+    end
   end
 
   def show
+    @item = Item.find(params[:id])
+    @genre = @item.genre
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item_id)
+    else
+      render "edit"
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :introduction, :genre_id, :price, :is_active, :image)
   end
 end
