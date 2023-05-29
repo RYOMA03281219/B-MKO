51710ce93beaffb1cbe8b43b789995ce7457ffe9
EC2 Default User <ec2-user@ip-172-31-26-57.ap-northeast-1.compute.internal>
2023-05-29 23:52:01 +0000

    [update]publicのrootの記述を更新

--- config/routes.rb
+++ config/routes.rb
@@ -1,95 +1,43 @@
 Rails.application.routes.draw do
+  namespace :public do
+    get 'addresses/index'
+    get 'addresses/edit'
+    get 'addresses/create'
+    get 'addresses/update'
+    get 'addresses/destroy'
+  end
 # 管理者用
 # URL /admin/sign_in ...
-devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
-}
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
 
-namespace :admin do
-get '' => 'homes#top'
-resources :customers, only: [:index, :show, :edit, :update]
-resources :orders, only: [:show, :update]
-resources :genres, only: [:index, :create, :edit, :update]
-end
+  namespace :admin do
+    get '' => 'homes#top'
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:show, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
+  end
 
-  # 顧客用
-# URL /customers/sign_in ...
-devise_for :customers,skip: [:passwords], controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
-}
+    # 顧客用
+  # URL /customers/sign_in ...
+  devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
 
-scope module: :public do
-root 'homes#top'
-get '/homes/about' => 'homes#about'
-
-
-
-end
-
-  # devise_for :customers
-  # devise_for :admins
-  #   get 'genres/index'
-  #   get 'genres/create'
-  #   get 'genres/edit'
-  #   get 'genres/update'
-  # end
-  # namespace :admin do
-  #   get 'orders/show'
-  #   get 'orders/update'
-  # end
-  # namespace :admin do
-  #   get 'customers/index'
-  #   get 'customers/show'
-  #   get 'customers/edit'
-  #   get 'customers/update'
-  # end
-  # namespace :admin do
-  #   get 'homes/top'
-  # end
-  # namespace :admin do
-  #   get 'sessions/new'
-  #   get 'sessions/create'
-  #   get 'sessions/destroy'
-  # end
-  # namespace :public do
-  #   get 'orders/new'
-  #   get 'orders/confirm'
-  #   get 'orders/completion'
-  #   get 'orders/create'
-  #   get 'orders/index'
-  #   get 'orders/show'
-  # end
-  # namespace :public do
-  #   get 'cart_items/index'
-  #   get 'cart_items/update'
-  #   get 'cart_items/destroy'
-  #   get 'cart_items/destroy_all'
-  #   get 'cart_items/create'
-  # end
-  # namespace :public do
-  #   get 'customers/show'
-  #   get 'customers/edit'
-  #   get 'customers/update'
-  #   get 'customers/cancellation'
-  #   get 'customers/withdrawal'
-  # end
-  # namespace :public do
-  #   get 'sessions/new'
-  #   get 'sessions/create'
-  #   get 'sessions/destroy'
-  # end
-  # namespace :public do
-  #   get 'registrations/new'
-  #   get 'registrations/create'
-  # end
-  # namespace :public do
-  #   get 'items/index'
-  #   get 'items/show'
-  # end
-  # namespace :public do
-  #   get 'homes/top'
-  #   get 'homes/about'
-  # end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope module: :public do
+    root 'homes#top'
+    get '/homes/about' => 'homes#about'
+    get '/customers/cancellation' => 'customers#cancellation'
+    patch '/customers/withdrawal' => 'customers#withdrawal'
+    resources :customer, only:[:show, :edit, :update]
+    delete '/cart_items/destroy_all' => 'cart_items#destroy_all'
+    resources :cart_items, only:[:index, :update, :destroy, :create]
+    resources :items, only:[:index, :new, :create, :show, :edit, :update]
+    post '/orders/confirm' => 'orders#confirm'
+    get '/orders/completion' => 'orders#completion'
+    resources :orders, only:[:new, :create, :index, :show]
+    resources :addresses, only:[:index, :edit, :create, :update, :destroy]
+  end
 end
