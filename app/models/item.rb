9bd016cae080ac108d1992fc512b0b57e9cb998e
EC2 Default User <ec2-user@ip-172-31-26-57.ap-northeast-1.compute.internal>
2023-06-01 20:35:09 +0000

    [Add]validateの追加、genre,order_detailsの連携、画像を投稿する際の記述追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,22 @@
 class Item < ApplicationRecord
+  validates :image, presence: {message: '選択されていません'}
+  validates :name, presence: true
+  validates :introducton, presence: true
+  validates :price, numericality: {only_integer: true}
+
+  belongs_to :genre
+  has_many :order_details
+  has_one_attached :image
+
+  def get_image(width, height)
+    unless image.attached?
+      file_path = Rails.root.join('./app/assets/images/default-image.jpg')
+      image.attach(io:File.open(file_path), filename: 'default-image.jpg',content_type: 'image/jpeg')
+    end
+    image.variant(resize_to_limit: [width, height]).processed
+  end
+
+  def with_tax_price
+    (price * 1.1).floor
+  end
 end
