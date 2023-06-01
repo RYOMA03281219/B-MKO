38856a212c711ae785c42e96e8c76cfbe7c1b5dc
EC2 Default User <ec2-user@ip-172-31-26-57.ap-northeast-1.compute.internal>
2023-06-01 20:34:22 +0000

    [Add]order,addressとの連携と退会したUSは再ログインできぬように記述追加

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -8,4 +8,11 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :orders
+  has_many :addresses
+
+  def active_for_authentication?
+    super && (is_deleted == false)
+  end
 end
