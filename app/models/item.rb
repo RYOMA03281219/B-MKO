a49229419fd743e9a4348622fbb72a58bc80b038
EC2 Default User <ec2-user@ip-172-31-26-57.ap-northeast-1.compute.internal>
2023-06-03 14:17:45 +0000

    [Add]デフォルトの画像の指定

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,8 +10,8 @@ class Item < ApplicationRecord
 
   def get_image(width, height)
     unless image.attached?
-      file_path = Rails.root.join('./app/assets/images/default-image.jpg')
-      image.attach(io:File.open(file_path), filename: 'default-image.jpg',content_type: 'image/jpeg')
+      file_path = Rails.root.join('./app/assets/images/B-MKO.jpg')
+      image.attach(io:File.open(file_path), filename: 'B-MKO.jpg',content_type: 'image/jpeg')
     end
     image.variant(resize_to_limit: [width, height]).processed
   end
