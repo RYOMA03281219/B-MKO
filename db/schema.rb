9c04d0cac9647f11faf704f3acdf94ed73226f52
EC2 Default User <ec2-user@ip-172-31-26-57.ap-northeast-1.compute.internal>
2023-06-12 17:32:10 +0000

    [Add]

--- db/schema.rb
+++ db/schema.rb
@@ -58,11 +58,7 @@ ActiveRecord::Schema.define(version: 2023_06_02_114357) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "last_name", null: false
-    t.string "first_name", null: false
-    t.string "postal_code", null: false
-    t.string "address", null: false
-    t.string "telephone_number", null: false
+    t.string "name", null: false
     t.boolean "is_delete", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
