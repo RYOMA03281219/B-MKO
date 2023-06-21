6b25357bb0227e47d9a70e319d381fd94c350809
EC2 Default User <ec2-user@ip-172-31-26-57.ap-northeast-1.compute.internal>
2023-06-21 15:05:43 +0000

    [Add]

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -3,4 +3,8 @@ class Admin::HomesController < ApplicationController
   def top
     @items = Item.all.page(params[:page]).per(5)
   end
+
+   def index
+    @categories = Category.where(id: Category.pluck(:ancestry).compact.map { |e| e.split('/') }.flatten.uniq)
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
+  module ApplicationHelper
   def sidebar_link_item(name, path)
     class_name = 'channel'
-    class_name << 'active' if current_page?(path)
+    class_name << ' active' if current_page?(path)
 
     content_tag :li, class:class_name do
       link_to name, path, class: 'channel_name'
     end
   end
 end
-
-
+end
