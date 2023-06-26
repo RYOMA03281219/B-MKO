eebf87cb929cd8d325c3564bb5b669b567fcca5b
EC2 Default User <ec2-user@ip-172-31-26-57.ap-northeast-1.compute.internal>
2023-06-26 13:23:43 +0000

    [Update]noimage画像が表示できるように記述の修正、不要なpriceの記述を削除

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,13 +12,9 @@ class Item < ApplicationRecord
 
   def get_image(width, height)
     unless image.attached?
-      file_path = Rails.root.join('./app/assets/images/B-MKO.jpg')
-      image.attach(io:File.open(file_path), filename: 'B-MKO.jpg',content_type: 'image/jpeg')
+      file_path = Rails.root.join('./app/assets/images/noimage.jpg')
+      image.attach(io:File.open(file_path), filename: 'noimage.jpg',content_type: 'image/jpeg')
     end
     image.variant(resize_to_limit: [width, height]).processed
-  end
-
-  def with_tax_price
-    (price * 1.1).floor
   end
 end
