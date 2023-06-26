662188ba73d5e37b1e5d899fabdcbd3692dd4396
EC2 Default User <ec2-user@ip-172-31-26-57.ap-northeast-1.compute.internal>
2023-06-26 22:06:42 +0000

    [Add]def deleteを追加

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -5,7 +5,7 @@ class Admin::GenresController < ApplicationController
   end
 
   def create
-    @genre = Genre.new(gemre_params)
+    @genre = Genre.new(genre_params)
     if @genre.save
       redirect_to admin_genres_path
     else
@@ -26,7 +26,14 @@ class Admin::GenresController < ApplicationController
     end
   end
 
+  def destroy
+    @genre = Genre.find(params[:id])
+    @genre.destroy
+    redirect_to request.referer
+  end
+
   private
+
   def genre_params
     params.require(:genre).permit(:name)
   end
