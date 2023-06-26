d409e4958e2d1edd286c6294311a87dbb75367ca
EC2 Default User <ec2-user@ip-172-31-26-57.ap-northeast-1.compute.internal>
2023-06-26 22:07:42 +0000

    [Add]各アクションへの記述を追加

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,10 +1,29 @@
 class Admin::ItemsController < ApplicationController
-  def show
+  def index
+    @items = Item.all.page(params[:page]).per(10)
   end
 
   def edit
+    @item = Item.find(params[:id])
+  end
+
+  def show
+    @item = Item.find(params[:id])
+    @genre = @item.genre
   end
 
   def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to admin_item_path(@item.id)
+    else
+      render "edit"
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :introduction, :genre_id)
   end
 end
