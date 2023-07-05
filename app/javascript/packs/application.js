20de509900d71f3c28278b97cd3b976e06a286e2
EC2 Default User <ec2-user@ip-172-31-26-57.ap-northeast-1.compute.internal>
2023-07-05 08:45:59 +0000

    [Add]jQueryとratyの追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,6 +13,13 @@ import "bootstrap";
 import '../stylesheets/application.scss'
 require('../script')
 require('../jquery.min')
+window.$ = window.jQuery = require('jquery');
+import Raty from "raty.js"
+window.raty = function(elem,opt) {
+  let raty =  new Raty(elem,opt)
+  raty.init();
+  return raty;
+}
 
 Rails.start()
 Turbolinks.start()
