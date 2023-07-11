6771a74f208da191554ee0536eb2114c32a26b3c
EC2 Default User <ec2-user@ip-172-31-26-57.ap-northeast-1.compute.internal>
2023-07-11 10:45:41 +0000

    [Update]顧客側のsign_inについて記述

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # def after_sign_in_path_for(resource)
-  #   customer_session_path(resource)
-  # end
+  def after_sign_in_path_for(resource)
+    customers_my_page_path
+  end
 
   # def after_sign_out_path_for(resource)
   #   root_path
@@ -12,6 +12,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :postal_code, :address, :telephone_number])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password])
   end
 end
