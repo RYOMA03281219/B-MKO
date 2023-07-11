0a41b6549299004643cfee81fbaf4496673d44e2
EC2 Default User <ec2-user@ip-172-31-26-57.ap-northeast-1.compute.internal>
2023-07-11 16:51:12 +0000

    [Add]destroyアクションを追加

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -34,9 +34,15 @@ class Public::ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    @item = Item.find(params[:id])
+    item.destroy
+    redirect_to items_path
+  end
+
   private
 
   def item_params
-    params.require(:item).permit(:name, :introduction, :genre_id, :review, :favorite, :image)
+    params.require(:item).permit(:name, :introduction, :genre_id, :review, :favorite, :image, :star)
   end
 end
