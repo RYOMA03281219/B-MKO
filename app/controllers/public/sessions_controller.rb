d5033777af69b57cc6d97345fee63c3d47a0cef6
EC2 Default User <ec2-user@ip-172-31-26-57.ap-northeast-1.compute.internal>
2023-07-16 13:09:29 +0000

    [Update]ログイン後の遷移先のリンクを変更

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,7 +1,7 @@
 class Public::SessionsController < Devise::SessionsController
   def after_sign_in_path_for(resource)
     flash[:welcome] = "ようこそ" + current_customer.last_name + "さん！"
-    customer_path
+    customers_my_page_path
   end
 
   def after_sign_out_path_for(resource)
