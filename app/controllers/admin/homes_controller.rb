4e5613542d1d023ad1c76b8f7e02d86fa794f609
EC2 Default User <ec2-user@ip-172-31-26-57.ap-northeast-1.compute.internal>
2023-07-19 12:56:38 +0000

    [Delete]def indexの記述を削除

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -3,8 +3,4 @@ class Admin::HomesController < ApplicationController
   def top
     @items = Item.all.page(params[:page]).per(5)
   end
-
-   def index
-    @categories = Category.where(id: Category.pluck(:ancestry).compact.map { |e| e.split('/') }.flatten.uniq)
-  end
 end
