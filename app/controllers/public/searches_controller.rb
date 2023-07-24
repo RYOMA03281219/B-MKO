55d5d848a67d4c7c1ddf828af00651150b05f071
EC2 Default User <ec2-user@ip-172-31-26-57.ap-northeast-1.compute.internal>
2023-07-24 14:48:46 +0000

    [Add]検索機能用のファイルと記述の追加

---
+++ app/controllers/public/searches_controller.rb
@@ -0,0 +1,14 @@
+class Public::SearchesController < ApplicationController
+   before_action :authenticate_customer!
+
+  def search_result
+    @range = params[:range]
+    @search_word = params[:search_word]
+
+    if @range == "Customer"
+      @customers = Customer.where("name LIKE?","%#{@search_word}%").where('is_delete', false)
+    else
+      @items = Item.where("name LIKE?","%#{@search_word}%")
+    end
+  end
+end
